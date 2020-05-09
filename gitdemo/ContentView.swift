94adcf055d06e2773be44a3eb7dc84237e1e70c1
Simon Rofe <simon.rofe@airbus.com>
2020-05-09 20:42:08 +1000

    Initial commit of new project

---
+++ gitdemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gitdemo
+//
+//  Created by Simon Rofe on 9/5/20.
+//  Copyright © 2020 Simon Rofe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
